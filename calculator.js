15d722adeb22e748ae9b061dacfddfc28aee64e5
BobiMarcianoGilburd <bam238@student.hks.harvard.edu>
2018-01-07 19:12:48 -0800

    add functions to calculator

---
+++ calculator.js
@@ -0,0 +1,32 @@
+// Calculator.js
+// This program defines four functions: multiply, add, subtract, and divide.
+// Each functions takes two arguments and return the operator's arithmetric result
+// (no output is printed to the screen).
+
+// multiply - this function multiplies two numbers and returns the result.
+// Input  : a, b
+// Returns: a * b
+function multiply(a, b) {
+	return a * b
+}
+
+// add - this function adds two numbers and returns the result.
+// Input  : a, b
+// Returns: a + b
+function add(a, b) {
+	return a + b
+}
+
+// subtract - this function subtracts two numbers and returns the result.
+// Input  : a, b
+// Returns: a - b
+function subtract(a, b) {
+	return a - b
+}
+
+// divide - this function divides two numbers and returns the result.
+// Input  : a, b
+// Returns: a / b
+function divide(a, b) {
+	return a / b
+}
