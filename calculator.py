c75fa4ca93c70d614a50a3ca2c3f167e1ce54ef8
BobiMarcianoGilburd <bam238@student.hks.harvard.edu>
2018-01-07 18:17:47 -0800

    add functions to calculator

---
+++ calculator.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python2
+
+# Calculator.py
+# This program defines four functions: multiply, add, subtract, and divide.
+# Each functions takes two arguments and return the operator's arithmetric result
+# (no output is printed to the screen).
+
+# multiply - this functions multiplies two numbers and returns the result.
+# Input  : a, b
+# Returns: a * b
+def multiply(a, b):
+	return a * b
+
+# add - this functions adds two numbers and returns the result.
+# Input  : a, b
+# Returns: a + b
+def add(a, b):
+	return a + b
+
+# substract - this functions substracts two numbers and returns the result.
+# Input  : a, b
+# Returns: a - b
+def substract(a, b):
+	return a - b
+
+# divide - this functions divides two numbers and returns the result.
+# Input  : a, b
+# Returns: a / b
+def substract(a, b):
+	return a / b
